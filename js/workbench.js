e42f10adb993e1c409f59e41b1aacb64c3ba0bdb
Andrea Agili <agea@drwolf.it>
2014-10-31 16:35:12 +0100

    fix

--- js/workbench.js
+++ js/workbench.js
@@ -1,5 +1,5 @@
 var wbconfig = wbconfig || {
-  url:'http://localhost:28080/alfresco/api/-default-/public/cmis/versions/1.1/browser',
+  url:'/alfresco/api/-default-/public/cmis/versions/1.1/browser',
   ok:function(data){$('#json').JSONView(data);}
   };
 
