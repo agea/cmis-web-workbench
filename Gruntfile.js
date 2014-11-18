1524a15735f8101e52c15f9939e2d9bd1e4fb1cf
Andrea Agili <agea@drwolf.it>
2014-11-18 11:09:32 +0100

    remote connection with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,22 @@
 /*global module:false*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
+  var host = "cmis.alfresco.com";
+  var port = 80;
+  var path = "/cmisbrowser";
+
+  if (process.argv.indexOf('--host') != -1) {
+    host = process.argv[process.argv.indexOf('--host') + 1];
+  }
+
+  if (process.argv.indexOf('--port') != -1) {
+    port = process.argv[process.argv.indexOf('--port') + 1];
+  }
+
+  if (process.argv.indexOf('--path') != -1) {
+    path = process.argv[process.argv.indexOf('--path') + 1];
+  }
 
   grunt.initConfig({
     bower_concat: {
@@ -23,6 +38,37 @@ module.exports = function(grunt) {
         dest: 'build/workbench.js'
       }
     },
+
+    connect: {
+      server: {
+        proxies: [
+          {
+            context: '/alfresco/api/-default-/public/cmis/versions/1.1/browser',
+            host: host,
+            port: port,
+            rewrite: {
+              '^/alfresco/api/-default-/public/cmis/versions/1.1/browser': path
+            },
+            changeOrigin: true
+          }
+        ],
+        options: {
+          port: 9000,
+          keepalive: true,
+          middleware: function (connect, options) {
+            var config = [
+                connect.static(options.base),
+                connect.directory(options.base)
+                ];
+            var proxy = require('grunt-connect-proxy/lib/utils').proxyRequest;
+            config.unshift(proxy);
+            return config;
+          }
+        },
+
+      }
+    },
+
     cssmin: {
       combine: {
         files: {
@@ -58,6 +104,11 @@ module.exports = function(grunt) {
     'bower_concat',
     'uglify:bower',
     'cssmin'
+  ]);
+
+  grunt.registerTask('server', [
+    'configureProxies:server',
+    'connect'
   ]);
 
   grunt.registerTask('buildwb', [
--- README.md
+++ README.md
@@ -12,6 +12,17 @@ Download [dist/workbench.html](https://github.com/agea/cmis-web-workbench/raw/ma
 
 (It will work on any iplementation as long as you are able to reach the CMIS endpont via ajax requests)
 
+Remote Usage
+-------------------------
+####You need to install node to connect to a remote server
+
+    $ git clone https://github.com/agea/cmis-web-workbench
+    $ cd cmis-web-workbench
+    $ npm install
+    $ grunt server --host SERVER --port PORT --path BROWSER_BINDING_PATH
+
+Then you can connect to http://localhost:9000
+
 License
 -------
 MIT license - [http://www.opensource.org/licenses/mit-license.php](http://www.opensource.org/licenses/mit-license.php)
