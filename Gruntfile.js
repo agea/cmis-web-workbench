0aafe842ce1051a50aaacf7d89ab67a3ad4d3ff5
Andrea Agili <agea@drwolf.it>
2015-01-14 08:39:13 +0100

    bower and grunt configuration cleaning

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,25 +19,6 @@ module.exports = function (grunt) {
   }
 
   grunt.initConfig({
-    bower_concat: {
-      all: {
-        dest: 'build/bower.js',
-        cssDest: 'build/bower.css',
-        mainFiles: {
-          'cmis': 'lib/cmis.js',
-          'jsonview': ['dist/jquery.jsonview.js', 'jquery.jsonview.css']
-        },
-        bowerOptions: {
-          relative: false
-        }
-      }
-    },
-    concat: {
-      dist: {
-        src: ['js/*', 'js/services/*', 'js/controllers/*'],
-        dest: 'build/workbench.js'
-      }
-    },
 
     connect: {
       server: {
@@ -69,24 +50,6 @@ module.exports = function (grunt) {
       }
     },
 
-    cssmin: {
-      combine: {
-        files: {
-          'bower.min.css': ['build/bower.css', 'app/bower_components/jquery-jsonview/dist/jquery.jsonview.css']
-        }
-      }
-    },
-    uglify: {
-      bower: {
-        options: {
-          mangle: true,
-          compress: true
-        },
-        files: {
-          'bower.min.js': 'build/bower.js'
-        }
-      }
-    },
     inline: {
       dist: {
         options: {
@@ -100,22 +63,12 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('buildbower', [
-    'bower_concat',
-    'uglify:bower',
-    'cssmin'
-  ]);
-
   grunt.registerTask('server', [
     'configureProxies:server',
     'connect'
   ]);
 
-  grunt.registerTask('buildwb', [
-    'concat:dist'
-  ]);
-
-  grunt.registerTask('buildinline', [
+  grunt.registerTask('dist', [
     'inline:dist'
   ]);
 
